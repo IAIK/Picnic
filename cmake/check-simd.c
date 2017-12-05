2975d7fc54709d0a368601657e419a747d284072
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2017-12-05 18:16:38 +0100

    Handle missing target attribute

--- cmake/check-simd.c
+++ cmake/check-simd.c
@@ -1,22 +1,28 @@
+#if defined(__GNUC__) || __has_attribute(target)
+#define ATTRIBUTE_TARGET(x) __attribute__((target((x))))
+#else
+#define ATTRIBUTE_TARGET(x)
+#endif
+
 #if defined(SSE2) || defined(SSE4_1) || defined(AVX2)
 #include <immintrin.h>
 
 #if defined(SSE2)
-__attribute__((target("sse2"))) void test(void) {
+ATTRIBUTE_TARGET("sse2") void test(void) {
   __m128i v = _mm_setzero_si128();
   (void)v;
 }
 #endif
 
 #if defined(SSE4_1)
-__attribute__((target("sse4.1"))) void test(void) {
+ATTRIBUTE_TARGET("sse4.1") void test(void) {
   __m128i v = _mm_setzero_si128();
   (void)_mm_testz_si128(v, v);
 }
 #endif
 
 #if defined(AVX2)
-__attribute__((target("avx2"))) void test(void) {
+ATTRIBUTE_TARGET("avx2") void test(void) {
   __m256i v = _mm256_setzero_si256();
   (void)v;
 }
