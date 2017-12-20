7a9a24970d6dc40d1ddc1c5e371c303378eecbcc
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2017-12-20 17:01:27 +0100

    Simplify bswap handling

--- endian_compat.h
+++ endian_compat.h
@@ -12,18 +12,41 @@
 
 #include <stdint.h>
 
+#if defined(__GCC__) || defined(__clang__)
+#define bswap16(x) __builtin_bswap16(x)
+#define bswap32(x) __builtin_bswap32(x)
+#define bswap64(x) __builtin_bswap64(x)
+#elif defined(_MSC_VER)
+#include <stdlib.h>
+
+#define bswap16(x) _byteswap_ushort(x)
+#define bswap32(x) _byteswap_ulong(x)
+#define bswap64(x) _byteswap_uint64(x)
+#else
+static inline uint16_t bswap16(uint16_t x) {
+  return ((x & 0xff00) >> 8) | ((x & 0x00ff) << 8);
+}
+
+static inline uint32_t bswap32(uint32_t x) {
+  return ((x & 0xff000000) >> 24) | ((x & 0x00ff0000) >> 8) | ((x & 0x0000ff00) << 8) |
+         ((x & 0x000000ff) << 24);
+}
+
+static inline uint64_t bswap64(uint64_t x) {
+  return ((x & UINT64_C(0xff00000000000000)) >> 56) | ((x & UINT64_C(0x00ff000000000000)) >> 40) |
+         ((x & UINT64_C(0x0000ff0000000000)) >> 24) | ((x & UINT64_C(0x000000ff00000000)) >> 8) |
+         ((x & UINT64_C(0x00000000ff000000)) << 8) | ((x & UINT64_C(0x0000000000ff0000)) << 24) |
+         ((x & UINT64_C(0x000000000000ff00)) << 40) | ((x & UINT64_C(0x00000000000000ff)) << 56);
+}
+#endif
+
 /* Linux / GLIBC */
 #if defined(__linux__) || defined(__GLIBC__)
-#include <byteswap.h>
 #include <endian.h>
-#define HAVE_BSWAP
 #define HAVE_HOSTSWAP
 #define _BYTE_ORDER __BYTE_ORDER
 #define _LITTLE_ENDIAN __LITTLE_ENDIAN
 #define _BIG_ENDIAN __BIG_ENDIAN
-#define bswap16(x) bswap_16(x)
-#define bswap32(x) bswap_32(x)
-#define bswap64(x) bswap_64(x)
 #endif
 
 /* Windows */
@@ -35,67 +58,21 @@
 #define _BIG_ENDIAN BIG_ENDIAN
 #define _BYTE_ORDER BYTE_ORDER
 #else
-#include <stdlib.h>
+
 #define _LITTLE_ENDIAN 1234
 #define _BIG_ENDIAN 4321
 /* X-Box 360 is big-endian, but we simply ignore that. */
 #define _BYTE_ORDER _LITTLE_ENDIAN
-#define HAVE_BSWAP
-#define bswap16(x) _byteswap_ushort(x)
-#define bswap32(x) _byteswap_ulong(x)
-#define bswap64(x) _byteswap_uint64(x)
 #endif
 #endif
 
 /* OS X */
 #if defined(__APPLE__)
 #include <machine/endian.h>
+
 #define _BYTE_ORDER BYTE_ORDER
 #define _LITTLE_ENDIAN LITTLE_ENDIAN
 #define _BIG_ENDIAN BIG_ENDIAN
-#endif
-
-#if !defined(_BYTE_ORDER) || !defined(_LITTLE_ENDIAN) || !defined(_BIG_ENDIAN)
-#error Could not determine CPU byte order
-#endif
-
-/* Byte swap macros */
-#if !defined(HAVE_BSWAP)
-
-#if !defined(bswap16)
-#if defined(__GNUC__)
-#define bswap16(x) __builtin_bswap16(x)
-#else
-static inline uint16_t bswap16(uint16_t x) {
-  return ((x & 0xff00) >> 8) | ((x & 0x00ff) << 8);
-}
-#endif
-#endif
-
-#if !defined(bswap32)
-#if defined(__GNUC__)
-#define bswap32(x) __builtin_bswap32(x)
-#else
-static inline uint32_t bswap32(uint32_t x) {
-  return ((x & 0xff000000) >> 24) | ((x & 0x00ff0000) >> 8) | ((x & 0x0000ff00) << 8) |
-         ((x & 0x000000ff) << 24);
-}
-#endif
-#endif
-
-#if !defined(bswap64)
-#if defined(__GNUC__)
-#define bswap64(x) __builtin_bswap64(x)
-#else
-static inline uint64_t bswap64(uint64_t x) {
-  return ((x & UINT64_C(0xff00000000000000)) >> 56) | ((x & UINT64_C(0x00ff000000000000)) >> 40) |
-         ((x & UINT64_C(0x0000ff0000000000)) >> 24) | ((x & UINT64_C(0x000000ff00000000)) >> 8) |
-         ((x & UINT64_C(0x00000000ff000000)) << 8) | ((x & UINT64_C(0x0000000000ff0000)) << 24) |
-         ((x & UINT64_C(0x000000000000ff00)) << 40) | ((x & UINT64_C(0x00000000000000ff)) << 56);
-}
-#endif
-#endif
-
 #endif
 
 #if !defined(HAVE_HOSTSWAP)
