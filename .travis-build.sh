9d6b6fed6e6eab21236b024d78c3c3ef1c3a031f
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-02-06 14:02:27 +0100

    Allow to set cmake generator

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,13 +4,23 @@ set -e
 directory=build-$(sha1sum <<< "$@" | awk '{print $1}')
 set -x
 
+function run_cmake
+{
+  if [[ -n "$CMAKE_GENERATOR" ]]
+  then
+    cmake -G "$CMAKE_GENERATOR" "$@"
+  else
+    cmake "$@"
+  fi
+}
+
 mkdir -p "$directory"
 cd "$directory"
 if [[ $# -eq 1 ]] && [[ -z $1 ]]
 then
-  cmake ..
+  run_cmake ..
 else
-  cmake .. "$@"
+  run_cmake .. "$@"
 fi
 cd ..
 
