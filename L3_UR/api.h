63d9f30d849b232933d5e1a88738199a6dd6087f
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-03-15 11:01:30 +0100

    Add #ifdef-ery to headers

--- L3_UR/api.h
+++ L3_UR/api.h
@@ -1,3 +1,6 @@
+#ifndef L3_UR_API_H
+#define L3_UR_API_H
+
 #define CRYPTO_SECRETKEYBYTES (1 + 2 * 24 + 24)
 #define CRYPTO_PUBLICKEYBYTES (1 + 2 * 24)
 #define CRYPTO_BYTES (4 + 121813)
@@ -8,3 +11,5 @@ int crypto_sign(unsigned char* sm, unsigned long long* smlen, const unsigned cha
                 unsigned long long mlen, const unsigned char* sk);
 int crypto_sign_open(unsigned char* m, unsigned long long* mlen, const unsigned char* sm,
                      unsigned long long smlen, const unsigned char* pk);
+
+#endif
