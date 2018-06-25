6cfc3abeccee048ab947d46cca59ea4d484a1bbb
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-06-25 20:27:03 +0200

    Define some fallbacks for macros

--- macros.h
+++ macros.h
@@ -19,6 +19,7 @@
 #define __has_builtin(b) 0
 #endif
 
+/* gcc version check macro */
 #if defined(__GNUC__) && defined(__GNUC_MINOR__)
 #define GNUC_CHECK(maj, min)                                                                       \
   (((__GNUC__ << 20) + (__GNUC_MINOR__ << 10)) >= (((maj) << 20) + ((min) << 10)))
@@ -34,6 +35,7 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #endif
 
+/* nonnull attribute */
 #if GNUC_CHECK(3, 3) || __has_attribute(nonnull)
 #define ATTR_NONNULL __attribute__((nonnull))
 #define ATTR_NONNULL_ARG(i) __attribute__((nonnull(i)))
@@ -42,30 +44,47 @@
 #define ATTR_NONNULL_ARG(i)
 #endif
 
+/* destructor attribute */
 #if GNUC_CHECK(2, 7) || __has_attribute(destructor)
 #define ATTR_DTOR __attribute__((destructor))
 #else
 #define ATTR_DTOR
 #endif
 
+/* assumed aligned attribute */
 #if GNUC_CHECK(4, 9) || __has_attribute(assume_aligned)
 #define ATTR_ASSUME_ALIGNED(i) __attribute__((assume_aligned(i)))
 #else
 #define ATTR_ASSUME_ALIGNED(i)
 #endif
 
+/* aligned attribute */
+/* note that C11's alignas will only do the job once DR 444 is implemented */
 #if GNUC_CHECK(4, 9) || __has_attribute(aligned)
-#define ATTR_ALIGNED(i) __attribute__((aligned(i)))
+#define ATTR_ALIGNED(i) __attribute__((aligned((i))))
+#elif defined(_MSC_VER)
+#define ATTR_ALIGNED(i) __declspec(align((i)))
 #else
 #define ATTR_ALIGNED(i)
 #endif
 
+/* unreachable builtin */
+#if GNUC_CHECK(4, 5) ||  __has_builtin(__builtin_unreachable)
+#define UNREACHABLE __builtin_unreachable()
+#elif defined(_MSC_VER)
+#define UNREACHABLE __assume(0)
+#endif
+
+/* assume aligned builtin */
 #if GNUC_CHECK(4, 9) || __has_builtin(__builtin_assume_aligned)
 #define ASSUME_ALIGNED(p, a) __builtin_assume_aligned((p), (a))
+#elif defined(UNREACHABLE)
+#define ASSUME_ALIGNED(p, a) (((((uintptr_t)(p)) % (a)) == 0) ? (p) : (UNREACHABLE, (p)))
 #else
 #define ASSUME_ALIGNED(p, a) (p)
 #endif
 
+/* always inline attribute */
 #if GNUC_CHECK(4, 0) || __has_attribute(always_inline)
 #define ATTR_ALWAYS_INLINE __attribute__((always_inline))
 #elif defined(_MSC_VER)
@@ -74,12 +93,14 @@
 #define ATTR_ALWAYS_INLINE
 #endif
 
+/* pure attribute */
 #if defined(__GNUC__) || __has_attribute(pure)
 #define ATTR_PURE __attribute__((pure))
 #else
 #define ATTR_PURE
 #endif
 
+/* target attribute */
 #if defined(__GNUC__) || __has_attribute(target)
 #define ATTR_TARGET(x) __attribute__((target((x))))
 #else
