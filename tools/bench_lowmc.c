adb72b0148c31ffa3649cf18e7a39cc9c8fb8e99
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-08-25 16:00:04 +0200

    Benchmark LowMC

---
+++ tools/bench_lowmc.c
@@ -0,0 +1,88 @@
+/*
+ *  This file is part of the optimized implementation of the Picnic signature scheme.
+ *  See the accompanying documentation for complete details.
+ *
+ *  The code is provided under the MIT license, see LICENSE for
+ *  more details.
+ *  SPDX-License-Identifier: MIT
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "bench_timing.h"
+#include "bench_utils.h"
+#include "io.h"
+#include "lowmc.h"
+#include "picnic_impl.h"
+#include "randomness.h"
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_timings(const uint64_t* timing, unsigned int iter) {
+  for (unsigned int i = 0; i < iter; i++) {
+    printf("%" PRIu64 "\n", timing[i]);
+  }
+}
+
+static void bench_lowmc(const bench_options_t* options) {
+  uint64_t* timings = calloc(options->iter, sizeof(uint64_t));
+
+  timing_context_t ctx;
+  if (!timing_init(&ctx)) {
+    printf("Failed to initialize timing functionality.\n");
+    return;
+  }
+
+  const picnic_instance_t* pp = get_instance(options->params);
+  if (!pp) {
+    printf("Failed to initialize LowMC instance.\n");
+    return;
+  }
+
+  const lowmc_t* lowmc                    = pp->lowmc;
+  const lowmc_implementation_f lowmc_impl = pp->lowmc_impl;
+
+  mzd_local_t* sk = mzd_local_init(1, lowmc->k);
+  mzd_local_t* pt = mzd_local_init(1, lowmc->n);
+
+  uint8_t* rand = malloc(pp->input_size + pp->output_size);
+  rand_bytes(rand, pp->input_size + pp->output_size);
+  mzd_from_char_array(sk, rand, pp->input_size);
+  mzd_from_char_array(pt, rand + pp->input_size, pp->output_size);
+  free(rand);
+
+  for (unsigned int i = 0; i != options->iter; ++i) {
+    const uint64_t start_time = timing_read(&ctx);
+
+    mzd_local_t* ct = lowmc_impl(lowmc, sk, pt);
+    mzd_local_free(pt);
+    pt = ct;
+
+    const uint64_t end_time = timing_read(&ctx);
+    timings[i]              = end_time - start_time;
+  }
+
+  mzd_local_free(pt);
+  mzd_local_free(sk);
+
+  timing_close(&ctx);
+  print_timings(timings, options->iter);
+
+  free(timings);
+}
+
+int main(int argc, char** argv) {
+  bench_options_t opts = {0};
+  int ret              = parse_args(&opts, argc, argv) ? 0 : -1;
+
+  if (!ret) {
+    bench_lowmc(&opts);
+  }
+
+  return ret;
+}
