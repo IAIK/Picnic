10af02690bffd3e9f0f5ccb608c633ab6a86963a
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-08-25 15:59:51 +0200

    Refactor option parsing

--- tools/bench.c
+++ tools/bench.c
@@ -14,19 +14,12 @@
 #include "picnic.h"
 #include "timing.h"
 #include "bench_timing.h"
+#include "bench_utils.h"
 
-#include <errno.h>
-#if !defined(_MSC_VER)
-#include <getopt.h>
-#endif
 #include <inttypes.h>
-#include <limits.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #ifndef VERBOSE
 static void print_timings(timing_and_size_t* timings, unsigned int iter) {
@@ -78,115 +71,7 @@ static void print_timings(timing_and_size_t* timings, unsigned int iter) {
 #endif
 #endif
 
-static bool parse_long(long* value, const char* arg) {
-  errno        = 0;
-  const long v = strtol(arg, NULL, 10);
-
-  if ((errno == ERANGE && (v == LONG_MAX || v == LONG_MIN)) || (errno != 0 && v == 0)) {
-    return false;
-  }
-  *value = v;
-
-  return true;
-}
-
-static bool parse_uint32_t(uint32_t* value, const char* arg) {
-  long tmp = 0;
-  if (!parse_long(&tmp, arg)) {
-    return false;
-  }
-
-  if (tmp < 0 || (unsigned long)tmp > UINT32_MAX) {
-    return false;
-  }
-
-  *value = tmp;
-  return true;
-}
-
-typedef struct {
-  picnic_params_t params;
-  uint32_t iter;
-} bench_options_t;
-
-static void print_usage(const char* arg0) {
-#if defined(_MSC_VER)
-  printf("usage: %s iterations instance\n", arg0);
-#else
-  printf("usage: %s [-i iterations] instance\n", arg0);
-#endif
-}
-
-static bool parse_args(bench_options_t* options, int argc, char** argv) {
-  if (argc <= 1) {
-    print_usage(argv[0]);
-    return false;
-  }
-
-  options->params = PARAMETER_SET_INVALID;
-  options->iter   = 10;
-
-#if !defined(_MSC_VER)
-  static const struct option long_options[] = {{"iter", required_argument, 0, 'i'}, {0, 0, 0, 0}};
-
-  int c            = -1;
-  int option_index = 0;
-
-  while ((c = getopt_long(argc, argv, "i:", long_options, &option_index)) != -1) {
-    switch (c) {
-    case 'i':
-      if (!parse_uint32_t(&options->iter, optarg)) {
-        printf("Failed to parse argument as positive base-10 number!\n");
-        return false;
-      }
-      break;
-
-    case '?':
-    default:
-      printf("usage: %s [-i iter] param\n", argv[0]);
-      return false;
-    }
-  }
-
-  if (optind == argc - 1) {
-    uint32_t p = -1;
-    if (!parse_uint32_t(&p, argv[optind])) {
-      printf("Failed to parse argument as positive base-10 number!\n");
-      return false;
-    }
-
-    if (p <= PARAMETER_SET_INVALID || p >= PARAMETER_SET_MAX_INDEX) {
-      printf("Invalid parameter set selected!\n");
-      return false;
-    }
-    options->params = p;
-  } else {
-    print_usage(argv[0]);
-    return false;
-  }
-#else
-  if (argc != 3) {
-    print_usage(argv[0]);
-    return false;
-  }
-
-  uint32_t p = -1;
-  if (!parse_uint32_t(&options->iter, argv[1]) || !parse_uint32_t(&p, argv[2])) {
-    printf("Failed to parse argument as positive base-10 number!\n");
-    return false;
-  }
-
-  if (p <= PARAMETER_SET_INVALID || p >= PARAMETER_SET_MAX_INDEX) {
-    printf("Invalid parameter set selected!\n");
-    return false;
-  }
-  options->params = p;
-#endif
-
-  return true;
-}
-
-static void sign_and_verify(const bench_options_t* options) {
+static void bench_sign_and_verify(const bench_options_t* options) {
   static const uint8_t m[] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
                               17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
 
@@ -258,11 +143,11 @@ static void sign_and_verify(const bench_options_t* options) {
 }
 
 int main(int argc, char** argv) {
-  bench_options_t opts;
+  bench_options_t opts = { 0 };
   int ret = parse_args(&opts, argc, argv) ? 0 : -1;
 
   if (!ret) {
-    sign_and_verify(&opts);
+    bench_sign_and_verify(&opts);
   }
 
   return ret;
---
+++ tools/bench_utils.h
@@ -0,0 +1,26 @@
+/*
+ *  This file is part of the optimized implementation of the Picnic signature scheme.
+ *  See the accompanying documentation for complete details.
+ *
+ *  The code is provided under the MIT license, see LICENSE for
+ *  more details.
+ *  SPDX-License-Identifier: MIT
+ */
+
+#ifndef BENCH_UTILS_H
+#define BENCH_UTILS_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "picnic.h"
+
+typedef struct {
+  picnic_params_t params;
+  uint32_t iter;
+  bool lowmc;
+} bench_options_t;
+
+bool parse_args(bench_options_t* options, int argc, char** argv);
+
+#endif
