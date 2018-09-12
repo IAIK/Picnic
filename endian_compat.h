42ee603ab8fa74726affda93d54c6a5aa69ce342
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2018-09-12 16:49:36 +0200

    Fix check

--- endian_compat.h
+++ endian_compat.h
@@ -12,7 +12,7 @@
 
 #include <stdint.h>
 
-#if defined(__GCC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
 #define bswap16(x) __builtin_bswap16(x)
 #define bswap32(x) __builtin_bswap32(x)
 #define bswap64(x) __builtin_bswap64(x)
