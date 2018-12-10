8e229385cd637d39021e2c64e9ff878d66d73ece
Daniel Kales <daniel.kales@tugraz.at>
2018-12-10 12:17:20 +0100

    document shuffle functions

--- mzd_additional.h
+++ mzd_additional.h
@@ -192,6 +192,9 @@ void mzd_addmul_vl_neon(mzd_local_t* c, mzd_local_t const* v, mzd_local_t const*
  */
 mzd_local_t* mzd_precompute_matrix_lookup(mzd_local_t const* A) ATTR_NONNULL;
 
+/**
+ * Shuffle vector x according to info in mask. Needed for OLLE optimiztaions.
+ */
 void mzd_shuffle_30(mzd_local_t* x, word mask) ATTR_NONNULL;
 void mzd_shuffle_3(mzd_local_t* x, word mask) ATTR_NONNULL;
 void mzd_shuffle_pext_30(mzd_local_t* x, word mask) ATTR_NONNULL;
