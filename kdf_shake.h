45d8b40067af8588cdad3c31aa77f45156c1fa49
Daniel Kales <daniel.kales@tugraz.at>
2019-01-16 13:18:13 +0100

    x4 version of KDF

--- kdf_shake.h
+++ kdf_shake.h
@@ -27,6 +27,7 @@
 
 #if !defined(SUPERCOP)
 #include "sha3/KeccakHash.h"
+#include "sha3/KeccakHashtimes4.h"
 #else
 #include <libkeccak.a.headers/KeccakHash.h>
 #endif
@@ -70,4 +71,42 @@ typedef Keccak_HashInstance kdf_shake_t;
 #define kdf_shake_get_randomness(ctx, dst, count) hash_squeeze((ctx), (dst), (count))
 #define kdf_shake_clear(ctx)
 
+// Instances that work with 4 states in parallel
+typedef Keccak_HashInstancetimes4 hash_context_x4;
+
+static inline void hash_init_x4(hash_context_x4* ctx, const picnic_instance_t* pp) {
+  if (pp->digest_size == 32) {
+    Keccak_HashInitializetimes4_SHAKE128(ctx);
+  } else {
+    Keccak_HashInitializetimes4_SHAKE256(ctx);
+  }
+}
+
+static inline void hash_update_x4(hash_context_x4* ctx, const uint8_t** data, size_t size) {
+  Keccak_HashUpdatetimes4(ctx, data, size << 3);
+}
+
+static inline void hash_init_prefix_x4(hash_context_x4* ctx, const picnic_instance_t* pp,
+                                    const uint8_t prefix) {
+  hash_init_x4(ctx, pp);
+  const uint8_t* prefixes[] = {&prefix, &prefix, &prefix, &prefix};
+  hash_update_x4(ctx, prefixes, sizeof(prefix));
+}
+
+static inline void hash_final_x4(hash_context_x4* ctx) {
+  Keccak_HashFinaltimes4(ctx, NULL);
+}
+
+static inline void hash_squeeze_x4(hash_context_x4* ctx, uint8_t** buffer, size_t buflen) {
+  Keccak_HashSqueezetimes4(ctx, buffer, buflen << 3);
+}
+
+typedef Keccak_HashInstancetimes4 kdf_shake_x4_t;
+
+#define kdf_shake_x4_init(ctx, pp) hash_init_x4((ctx), (pp))
+#define kdf_shake_x4_init_prefix(ctx, pp, prefix) hash_init_prefix_x4((ctx), (pp), (prefix))
+#define kdf_shake_x4_update_key(ctx, key, keylen) hash_update_x4((ctx), (key), (keylen))
+#define kdf_shake_x4_finalize_key(ctx) hash_final_x4((ctx))
+#define kdf_shake_x4_get_randomness(ctx, dst, count) hash_squeeze_x4((ctx), (dst), (count))
+#define kdf_shake_x4_clear(ctx)
 #endif
