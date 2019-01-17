95153957a0de1210b7d551c3e0e708d5fac57bce
Daniel Kales <daniel.kales@tugraz.at>
2019-01-17 15:30:11 +0100

    Cleaned up config file

--- sha3/avx2x4/SIMD256-config.h
+++ sha3/avx2x4/SIMD256-config.h
@@ -5,12 +5,3 @@ This file defines some parameters of the implementation in the parent directory.
 #define KeccakP1600times4_implementation_config "AVX2, all rounds unrolled"
 #define KeccakP1600times4_fullUnrolling
 #define KeccakP1600times4_useAVX2
-/*
-This file defines some parameters of the implementation in the parent directory.
-*/
-
-/*
-#define KeccakP1600times4_implementation_config "AVX2, 12 rounds unrolled"
-#define KeccakP1600times4_unrolling 12
-#define KeccakP1600times4_useAVX2
-*/
