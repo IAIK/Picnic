9da3338d21299ea17bc7d1413f4e008448406400
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2019-08-06 09:48:56 +0200

    Fix assumption

--- lowmc.c
+++ lowmc.c
@@ -410,7 +410,11 @@ lowmc_store_implementation_f lowmc_store_get_implementation(const lowmc_t* lowmc
 }
 
 lowmc_compute_aux_implementation_f lowmc_compute_aux_get_implementation(const lowmc_t* lowmc) {
+#if defined(WITH_LOWMC_M1)
+  ASSUME(lowmc->m == 10 || lowmc->m == 1);
+#else
   ASSUME(lowmc->m == 10);
+#endif
   ASSUME(lowmc->n == 128 || lowmc->n == 192 || lowmc->n == 256);
 #if defined(WITH_OPT)
 #if defined(WITH_AVX2)
