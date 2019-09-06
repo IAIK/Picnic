8d674af0fc3f6ffebe1b2a745fb99a5049fba9a8
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2019-09-06 11:22:45 +0200

    Use kdf_shake interface

--- tests/shake_test.c
+++ tests/shake_test.c
@@ -10,16 +10,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "KeccakHash.h"
-#if defined(WITH_KECCAK_X4)
-#include "KeccakHashtimes4.h"
-#endif
+#include "kdf_shake.h"
 
 int main(void) {
   const uint8_t data1[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
   const uint8_t data2[8] = {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
 
-#if defined(WITH_KECCAK_X4)
   const uint8_t* data_ptrs[4] = {data1, data2, data2, data1};
   uint8_t output1[32]         = {0};
   uint8_t output2[32]         = {0};
@@ -27,62 +23,25 @@ int main(void) {
   uint8_t output4[32]         = {0};
   uint8_t* output_ptrs[4]     = {output1, output2, output3, output4};
 
-  Keccak_HashInstancetimes4 ctx;
-  if (Keccak_HashInitializetimes4_SHAKE256(&ctx)) {
-    printf("Initialization 4x failed.");
-    return -1;
-  }
-  if (Keccak_HashUpdatetimes4(&ctx, data_ptrs, 64)) {
-    printf("Update 4x failed");
-    return -1;
-  }
-  if (Keccak_HashFinaltimes4(&ctx, NULL)) {
-    printf("Final 4x failed");
-    return -1;
-  }
-  if (Keccak_HashSqueezetimes4(&ctx, output_ptrs, 256)) {
-    printf("Squeeze 4x failed");
-    return -1;
-  }
-#endif
+  hash_context_x4 ctx;
+  hash_init_x4(&ctx, 64);
+  hash_update_x4(&ctx, data_ptrs, 8);
+  hash_final_x4(&ctx);
+  hash_squeeze_x4(&ctx, output_ptrs, 32);
 
   uint8_t out1[32] = {0};
   uint8_t out2[32] = {0};
-  Keccak_HashInstance ctx1;
-  if (Keccak_HashInitialize_SHAKE256(&ctx1)) {
-    printf("Init 1 failed");
-    return -1;
-  }
-  if (Keccak_HashUpdate(&ctx1, data1, 64)) {
-    printf("Update 1 failed");
-    return -1;
-  }
-  if (Keccak_HashFinal(&ctx1, NULL)) {
-    printf("Final 1 failed");
-    return -1;
-  }
-  if (Keccak_HashSqueeze(&ctx1, out1, 256)) {
-    printf("Squeeze 1 failed");
-    return -1;
-  }
+  hash_context ctx1;
+  hash_init(&ctx1, 64);
+  hash_update(&ctx1, data1, 8);
+  hash_final(&ctx1);
+  hash_squeeze(&ctx1, out1, 32);
 
-  Keccak_HashInstance ctx2;
-  if (Keccak_HashInitialize_SHAKE256(&ctx2)) {
-    printf("Init 2 failed");
-    return -1;
-  }
-  if (Keccak_HashUpdate(&ctx2, data2, 64)) {
-    printf("Update 2 failed");
-    return -1;
-  }
-  if (Keccak_HashFinal(&ctx2, NULL)) {
-    printf("Final 2 failed");
-    return -1;
-  }
-  if (Keccak_HashSqueeze(&ctx2, out2, 256)) {
-    printf("Squeeze 2 failed");
-    return -1;
-  }
+  hash_context ctx2;
+  hash_init(&ctx2, 64);
+  hash_update(&ctx2, data2, 8);
+  hash_final(&ctx2);
+  hash_squeeze(&ctx2, out2, 32);
 
   const uint8_t expected_output1[32] = {0xd5, 0x9c, 0x54, 0x97, 0x73, 0xd2, 0xb3, 0x91,
                                         0x57, 0x49, 0x76, 0xb9, 0x59, 0xeb, 0xf6, 0xe0,
@@ -98,7 +57,6 @@ int main(void) {
   if (memcmp(out2, expected_output2, 32) != 0)
     return -1;
 
-#if defined(WITH_KECCAK_X4)
   if (memcmp(output1, expected_output1, 32) != 0)
     return -1;
   if (memcmp(output2, expected_output2, 32) != 0)
@@ -107,7 +65,6 @@ int main(void) {
     return -1;
   if (memcmp(output4, expected_output1, 32) != 0)
     return -1;
-#endif
 
   return 0;
 }
