29a07643efe72522e06f0a0a2ccb760566b59383
Daniel Kales <daniel.kales@iaik.tugraz.at>
2020-03-02 15:45:58 +0100

    fixed mzd io functions for arrays that are not multiples of 8

--- io.c
+++ io.c
@@ -17,22 +17,22 @@
 #include "compat.h"
 
 void mzd_to_char_array(uint8_t* dst, const mzd_local_t* data, size_t len) {
-  const size_t word_count = len / sizeof(uint64_t);
+  const size_t word_count = (len + 7) / sizeof(uint64_t);
   const block_t* block    = CONST_BLOCK(data, 0);
 
-  for (size_t i = word_count; i; --i, dst += sizeof(uint64_t)) {
+  for (size_t i = word_count; i; --i, dst += sizeof(uint64_t), len -= sizeof(uint64_t)) {
     const uint64_t tmp = htobe64(block->w64[i - 1]);
-    memcpy(dst, &tmp, sizeof(tmp));
+    memcpy(dst, &tmp, MIN(sizeof(tmp), len));
   }
 }
 
 void mzd_from_char_array(mzd_local_t* result, const uint8_t* data, size_t len) {
-  const size_t word_count = len / sizeof(uint64_t);
+  const size_t word_count = (len + 7) / sizeof(uint64_t);
   block_t* block          = BLOCK(result, 0);
 
-  for (size_t i = word_count; i; --i, data += sizeof(uint64_t)) {
-    uint64_t tmp;
-    memcpy(&tmp, data, sizeof(tmp));
+  for (size_t i = word_count; i; --i, data += sizeof(uint64_t), len -= sizeof(uint64_t)) {
+    uint64_t tmp = 0;
+    memcpy(&tmp, data, MIN(sizeof(tmp), len));
     block->w64[i - 1] = be64toh(tmp);
   }
 }
