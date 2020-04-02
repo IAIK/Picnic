989073f76fd300bf77b596b3d53da9a0b12b68d2
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2020-04-02 22:06:54 +0200

    Set key for visualize_signature with setBit

--- tools/visualize_signature.c
+++ tools/visualize_signature.c
@@ -3,6 +3,7 @@
 #endif
 
 #include "../picnic_impl.h"
+#include "../io.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,20 +17,24 @@ static int test_vector(const picnic_params_t param) {
 
   printf("Picnic test vectors for parameter set: %s\n", picnic_get_param_name(param));
 
-  const size_t lowmc_blocksize = picnic_get_lowmc_block_size(param);
-  if (lowmc_blocksize == 0) {
-    printf("invalid parameter set\n");
+  const picnic_instance_t* instance = picnic_instance_get(param);
+  if (!instance) {
+    printf("unsupported instance\n");
     return -1;
   }
 
-  picnic_privatekey_t sk = {0};
+  picnic_privatekey_t sk = {{0}};
   sk.data[0]             = param;
-  memset(&sk.data[1], 0x7c, lowmc_blocksize);
-  memset(&sk.data[1 + 2 * lowmc_blocksize], 0xf8, lowmc_blocksize);
+  for (unsigned int i = 0; i < instance->lowmc.k; i += 2) {
+    setBit(&sk.data[1], i, 1);
+  }
+  for (unsigned int i = 0; i < instance->lowmc.n; i += 3) {
+    setBit(&sk.data[1 + instance->input_size + instance->output_size], i, 1);
+  }
 
   picnic_publickey_t pk = {{0}};
   picnic_sk_to_pk(&sk, &pk);
-  memcpy(&sk.data[1 + lowmc_blocksize], &pk.data[1], lowmc_blocksize);
+  memcpy(&sk.data[1 + instance->input_size], &pk.data[1], instance->output_size);
 
   const uint8_t msg[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
 
