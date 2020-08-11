b115234188534d9d9cb58854710045ecfc395e13
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2020-08-11 10:07:38 +0200

    Add missing calling convention

--- picnic.c
+++ picnic.c
@@ -404,7 +404,7 @@ int PICNIC_CALLING_CONVENTION picnic_read_private_key(picnic_privatekey_t* key, 
   return 0;
 }
 
-void picnic_clear_private_key(picnic_privatekey_t* key) {
+void PICNIC_CALLING_CONVENTION picnic_clear_private_key(picnic_privatekey_t* key) {
 #if defined(HAVE_EXPLICIT_BZERO) || GLIBC_CHECK(2, 25)
   explicit_bzero(key, sizeof(picnic_privatekey_t));
 #else
