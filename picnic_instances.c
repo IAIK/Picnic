690906f87b9a8936e904827bc642bcdbb7c66a51
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2021-12-07 13:13:50 +0100

    Refactor parameter sets definition

--- picnic_instances.c
+++ picnic_instances.c
@@ -19,46 +19,40 @@
 #if defined(WITH_LOWMC_128_128_20)
 #include "lowmc_128_128_20.h"
 #else
-#define lowmc_parameters_128_128_20 { 0, 0, 0, 0 }
+#define lowmc_parameters_128_128_20                                                                \
+  { 0, 0, 0, 0 }
 #endif
 #if defined(WITH_LOWMC_192_192_30)
 #include "lowmc_192_192_30.h"
 #else
-#define lowmc_parameters_192_192_30 { 0, 0, 0, 0 }
+#define lowmc_parameters_192_192_30                                                                \
+  { 0, 0, 0, 0 }
 #endif
 #if defined(WITH_LOWMC_256_256_38)
 #include "lowmc_256_256_38.h"
 #else
-#define lowmc_parameters_256_256_38 { 0, 0, 0, 0 }
+#define lowmc_parameters_256_256_38                                                                \
+  { 0, 0, 0, 0 }
 #endif
 
 // L1, L3, and L5 instances with full Sbox layer
 #if defined(WITH_LOWMC_129_129_4)
 #include "lowmc_129_129_4.h"
 #else
-#define lowmc_parameters_129_129_4 { 0, 0, 0, 0 }
+#define lowmc_parameters_129_129_4                                                                 \
+  { 0, 0, 0, 0 }
 #endif
 #if defined(WITH_LOWMC_192_192_4)
 #include "lowmc_192_192_4.h"
 #else
-#define lowmc_parameters_192_192_4 { 0, 0, 0, 0 }
+#define lowmc_parameters_192_192_4                                                                 \
+  { 0, 0, 0, 0 }
 #endif
 #if defined(WITH_LOWMC_255_255_4)
 #include "lowmc_255_255_4.h"
 #else
-#define lowmc_parameters_255_255_4 { 0, 0, 0, 0 }
-#endif
-
-#if defined(WITH_ZKBPP)
-#define ENABLE_ZKBPP(x) x
-#else
-#define ENABLE_ZKBPP(x) { 0, 0, 0, 0 }
-#endif
-
-#if defined(WITH_KKW)
-#define ENABLE_KKW(x) x
-#else
-#define ENABLE_KKW(x) { 0, 0, 0, 0 }
+#define lowmc_parameters_255_255_4                                                                 \
+  { 0, 0, 0, 0 }
 #endif
 
 #if defined(WITH_ZKBPP) && defined(WITH_KKW)
@@ -74,34 +68,77 @@
 #error "At least one of WITH_ZKBPP and WITH_KKW have to be defined!"
 #endif
 
+#if defined(WITH_ZKBPP)
+#define PARAMETER_SET_ZKBPP(params, digest_size, seed_size, num_rounds, num_opened_rounds,         \
+                            input_size, output_size, view_size, view_round_size,                   \
+                            collapsed_challenge_size, unruh_without_input_bytes_size,              \
+                            unruh_with_input_bytes_size, max_signature_size)                       \
+  {                                                                                                \
+    params, digest_size, seed_size, num_rounds, num_opened_rounds, 3, input_size, output_size,     \
+        view_size, view_round_size, collapsed_challenge_size, unruh_without_input_bytes_size,      \
+        unruh_with_input_bytes_size, max_signature_size, NULL_FNS                                  \
+  }
+#else
+#define PARAMETER_SET_ZKBPP(params, digest_size, seed_size, num_rounds, num_opened_rounds,         \
+                            input_size, output_size, view_size, view_round_size,                   \
+                            collapsed_challenge_size, unruh_without_input_bytes_size,              \
+                            unruh_with_input_bytes_size, max_signature_size)                       \
+  {                                                                                                \
+    {0, 0, 0, 0}, digest_size, seed_size, num_rounds, num_opened_rounds, 3, input_size,            \
+        output_size, view_size, view_round_size, collapsed_challenge_size,                         \
+        unruh_without_input_bytes_size, unruh_with_input_bytes_size, max_signature_size, NULL_FNS  \
+  }
+#endif
+
+#if defined(WITH_KKW)
+#define PARAMETER_SET_KKW(params, digest_size, seed_size, num_rounds, num_opened_rounds,           \
+                          num_MPC_parties, input_size, output_size, view_size, view_round_size,    \
+                          collapsed_challenge_size, max_signature_size)                            \
+  {                                                                                                \
+    params, digest_size, seed_size, num_rounds, num_opened_rounds, num_MPC_parties, input_size,    \
+        output_size, view_size, view_round_size, collapsed_challenge_size, 0, 0,                   \
+        max_signature_size, NULL_FNS                                                               \
+  }
+#else
+#define PARAMETER_SET_KKW(params, digest_size, seed_size, num_rounds, num_opened_rounds,           \
+                          num_MPC_parties, input_size, output_size, view_size, view_round_size,    \
+                          collapsed_challenge_size, max_signature_size)                            \
+  {                                                                                                \
+    {0, 0, 0, 0}, digest_size, seed_size, num_rounds, num_opened_rounds, 3, input_size,            \
+        output_size, view_size, view_round_size, collapsed_challenge_size, 0, 0,                   \
+        max_signature_size, NULL_FNS                                                               \
+  }
+
+#endif
+
 static picnic_instance_t instances[PARAMETER_SET_MAX_INDEX - 1] = {
     /* ZKB++ with partial LowMC instances */
-    {ENABLE_ZKBPP(lowmc_parameters_128_128_20), 32, 16, 219, 219, 3, 16, 16, 75, 30, 55, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L1_FS, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_128_128_20), 32, 16, 219, 219, 3, 16, 16, 75, 30, 55, 91, 107,
-     PICNIC_SIGNATURE_SIZE_Picnic_L1_UR, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_192_192_30), 48, 24, 329, 329, 3, 24, 24, 113, 30, 83, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L3_FS, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_192_192_30), 48, 24, 329, 329, 3, 24, 24, 113, 30, 83, 137, 161,
-     PICNIC_SIGNATURE_SIZE_Picnic_L3_UR, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_256_256_38), 64, 32, 438, 438, 3, 32, 32, 143, 30, 110, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L5_FS, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_256_256_38), 64, 32, 438, 438, 3, 32, 32, 143, 30, 110, 175, 207,
-     PICNIC_SIGNATURE_SIZE_Picnic_L5_UR, NULL_FNS},
+    PARAMETER_SET_ZKBPP(lowmc_parameters_128_128_20, 32, 16, 219, 219, 16, 16, 75, 30, 55, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L1_FS),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_128_128_20, 32, 16, 219, 219, 16, 16, 75, 30, 55, 91, 107,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L1_UR),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_192_192_30, 48, 24, 329, 329, 24, 24, 113, 30, 83, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L3_FS),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_192_192_30, 48, 24, 329, 329, 24, 24, 113, 30, 83, 137,
+                        161, PICNIC_SIGNATURE_SIZE_Picnic_L3_UR),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_256_256_38, 64, 32, 438, 438, 32, 32, 143, 30, 110, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L5_FS),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_256_256_38, 64, 32, 438, 438, 32, 32, 143, 30, 110, 175,
+                        207, PICNIC_SIGNATURE_SIZE_Picnic_L5_UR),
     /* KKW with full LowMC instances */
-    {ENABLE_KKW(lowmc_parameters_129_129_4), 32, 16, 250, 36, 16, 17, 17, 65, 129, 55, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic3_L1, NULL_FNS},
-    {ENABLE_KKW(lowmc_parameters_192_192_4), 48, 24, 419, 52, 16, 24, 24, 96, 192, 83, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic3_L3, NULL_FNS},
-    {ENABLE_KKW(lowmc_parameters_255_255_4), 64, 32, 601, 68, 16, 32, 32, 128, 255, 110, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic3_L5, NULL_FNS},
+    PARAMETER_SET_KKW(lowmc_parameters_129_129_4, 32, 16, 250, 36, 16, 17, 17, 65, 129, 55,
+                      PICNIC_SIGNATURE_SIZE_Picnic3_L1),
+    PARAMETER_SET_KKW(lowmc_parameters_192_192_4, 48, 24, 419, 52, 16, 24, 24, 96, 192, 83,
+                      PICNIC_SIGNATURE_SIZE_Picnic3_L3),
+    PARAMETER_SET_KKW(lowmc_parameters_255_255_4, 64, 32, 601, 68, 16, 32, 32, 128, 255, 110,
+                      PICNIC_SIGNATURE_SIZE_Picnic3_L5),
     /* ZKB++ with full LowMC instances */
-    {ENABLE_ZKBPP(lowmc_parameters_129_129_4), 32, 16, 219, 219, 3, 17, 17, 65, 129, 55, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L1_full, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_192_192_4), 48, 24, 329, 329, 3, 24, 24, 96, 192, 83, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L3_full, NULL_FNS},
-    {ENABLE_ZKBPP(lowmc_parameters_255_255_4), 64, 32, 438, 438, 3, 32, 32, 128, 255, 110, 0, 0,
-     PICNIC_SIGNATURE_SIZE_Picnic_L5_full, NULL_FNS},
+    PARAMETER_SET_ZKBPP(lowmc_parameters_129_129_4, 32, 16, 219, 219, 17, 17, 65, 129, 55, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L1_full),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_192_192_4, 48, 24, 329, 329, 24, 24, 96, 192, 83, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L3_full),
+    PARAMETER_SET_ZKBPP(lowmc_parameters_255_255_4, 64, 32, 438, 438, 32, 32, 128, 255, 110, 0, 0,
+                        PICNIC_SIGNATURE_SIZE_Picnic_L5_full),
 };
 
 static bool create_instance(picnic_params_t params, picnic_instance_t* pp) {
