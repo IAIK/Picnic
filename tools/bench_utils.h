a4f5fcdb282961613414280e33dfdc37984b07a5
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2022-02-03 19:36:18 +0100

    Fix build with clang

--- tools/bench_utils.h
+++ tools/bench_utils.h
@@ -17,42 +17,40 @@
 
 #include "utils.h"
 
-namespace {
-  std::istream& operator>>(std::istream& in, picnic_params_t& param) {
-    std::string str;
-    in >> str;
-    param = argument_to_params(str.c_str());
-    if (param == PARAMETER_SET_INVALID) {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
+std::istream& operator>>(std::istream& in, picnic_params_t& param) {
+  std::string str;
+  in >> str;
+  param = argument_to_params(str.c_str());
+  if (param == PARAMETER_SET_INVALID) {
+    in.setstate(std::ios::failbit);
   }
+  return in;
+}
 
-  std::tuple<picnic_params_t, unsigned int> parse_args(int argc, char** argv) {
-    using namespace boost::program_options;
+std::tuple<picnic_params_t, unsigned int> parse_args(int argc, char** argv) {
+  using namespace boost::program_options;
 
-    options_description options{"Options"};
-    options.add_options()("help", "produce help message");
-    options.add_options()("iter,i", value<unsigned int>()->default_value(100),
-                          "set number of iterations");
-    options.add_options()("params,p", value<picnic_params_t>()->required(), "set parameter set");
+  options_description options{"Options"};
+  options.add_options()("help", "produce help message");
+  options.add_options()("iter,i", value<unsigned int>()->default_value(100),
+                        "set number of iterations");
+  options.add_options()("params,p", value<picnic_params_t>()->required(), "set parameter set");
 
-    variables_map vm;
-    try {
-      store(parse_command_line(argc, argv, options), vm);
-      notify(vm);
+  variables_map vm;
+  try {
+    store(parse_command_line(argc, argv, options), vm);
+    notify(vm);
 
-      if (vm.count("help")) {
-        std::cout << options << std::endl;
-        return std::make_tuple(PARAMETER_SET_INVALID, 0);
-      }
-
-      return std::make_tuple(vm["params"].as<picnic_params_t>(), vm["iter"].as<unsigned int>());
-    } catch (const boost::exception& e) {
+    if (vm.count("help")) {
       std::cout << options << std::endl;
       return std::make_tuple(PARAMETER_SET_INVALID, 0);
     }
+
+    return std::make_tuple(vm["params"].as<picnic_params_t>(), vm["iter"].as<unsigned int>());
+  } catch (const boost::exception& e) {
+    std::cout << options << std::endl;
+    return std::make_tuple(PARAMETER_SET_INVALID, 0);
   }
-} // namespace
+}
 
 #endif
